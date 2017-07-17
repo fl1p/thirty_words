1a57db111eec2b347253b7dbd017a795f59033c7
Christoph Zipperle <christoph@pace.car>
2017-07-17 21:29:55 +0200

    Set up static file directory

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 /* Settings */
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+/* Declare static directory */
+app.use(express.static(path.join(__dirname,'public')))
+
+/* Router */
 app.get('/', function (req, res) {
   console.log('*')
   res.render('main.pug')
