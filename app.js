82bd1cb5fb4a71c107ff075d915c432e4094bc6e
Christoph Zipperle <christoph@pace.car>
2017-07-17 22:33:24 +0200

    Set up main views, links and routes

--- app.js
+++ app.js
@@ -11,10 +11,21 @@ app.use(express.static(path.join(__dirname,'public')))
 
 /* Router */
 app.get('/', function (req, res) {
-  console.log('*')
+  console.log('Main')
   res.render('main.pug')
 })
 
+app.get('/search', function (req, res) {
+  console.log('Search')
+  res.render('search.pug')
+})
+
+app.get('/about', function (req, res) {
+  console.log('About')
+  res.render('about.pug')
+})
+
+/* Server */
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
 })
