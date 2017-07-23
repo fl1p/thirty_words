0cca657f9726590caad5bed1e6bab812a9ddc8c4
Christoph Zipperle <christoph@pace.car>
2017-07-23 19:43:19 +0200

    Add event listener to search form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
   console.log('Test')
 
-  // Captcha return keypress
-  // read input and
-  // log it to console
+  const input = document.querySelector('#searchInput')
+  let timer
+  const waitInterval = 2000
+
+  input.addEventListener('keyup', function () {
+    clearTimeout(timer)
+    timer = setTimeout(doneTyping, waitInterval)
+  })
+
+  input.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      doneTyping()
+    } else {
+      clearTimeout(timer)
+    }
+  })
+
+  function doneTyping () {
+    console.log(input.value)
+  }
 })
