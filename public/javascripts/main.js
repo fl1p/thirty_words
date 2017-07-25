96ca90393a32acb06959555e64c66e2b8e78ad69
Christoph Zipperle <christoph@pace.car>
2017-07-25 15:13:52 +0200

    Guard search form event listener

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-  console.log('Test')
-
   // SEARCH SOURCES
 
   // set up event listener to detect user input
   const input = document.querySelector('#searchInput')
-  let timer
-  const waitInterval = 2000
+  if (input) {
+    let timer
+    const waitInterval = 2000
 
-  input.addEventListener('keyup', function () {
-    clearTimeout(timer)
-    timer = setTimeout(searchSources, waitInterval)
-  })
-
-  // TODO somehow two calls to the wiki api are made instead of just one
-  input.addEventListener('keydown', function (e) {
-    if (e.keyCode === 13) {
-      e.preventDefault()
-      searchSources()
-    } else {
+    input.addEventListener('keyup', function () {
       clearTimeout(timer)
-    }
-  })
+      timer = setTimeout(searchSources, waitInterval)
+    })
+
+    // TODO somehow this creates two calls to the wiki api instead of just one
+    input.addEventListener('keydown', function (e) {
+      if (e.keyCode === 13) {
+        e.preventDefault()
+        searchSources()
+      } else {
+        clearTimeout(timer)
+      }
+    })
+  }
 })
