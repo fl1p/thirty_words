e36c7c6788da9effeeec100b23e35b22307fddd3
Christoph Zipperle <cz@bluecoderec.com>
2017-08-08 00:08:05 +0200

    Remove auto submit on search input

--- public/src/javascripts/main.js
+++ public/src/javascripts/main.js
@@ -6,21 +6,10 @@ $(document).ready(function () {
   // set up event listener to detect user input
   const input = document.querySelector('#search_input')
   if (input) {
-    let timer
-    const waitInterval = 2000
-
-    input.addEventListener('keyup', function () {
-      clearTimeout(timer)
-      timer = setTimeout(searchSources, waitInterval)
-    })
-
-    // TODO somehow this creates two calls to the wiki api instead of just one
     input.addEventListener('keydown', function (e) {
       if (e.keyCode === 13) {
         e.preventDefault()
         searchSources()
-      } else {
-        clearTimeout(timer)
       }
     })
   }
