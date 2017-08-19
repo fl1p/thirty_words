0862a2ab31fb65c6e1a7283e149956b1730a41b1
Christoph Zipperle <cz@bluecoderec.com>
2017-08-19 13:14:08 +0200

    Add adaptive font size for search input field

--- public/src/javascripts/modules/firstScreen.js
+++ public/src/javascripts/modules/firstScreen.js
@@ -1,24 +1,28 @@
 import { prepareSecondScreen } from './secondScreen'
 import { handleEmptyInput } from './firstScreenHelper'
+import { adaptFontSize } from './firstScreenHelper'
 
 export const showFirstScreen = function () {
   // reset focus to input when user closes modal
-  $('#searchHelpModal').on('hidden.bs.modal', function () {
+  $('#searchHelpModal').on('hidden.bs.modal', () => {
     $('#search_input').focus()
   })
 
-  // handle submit
+  // handle submit & typing
   const input = document.querySelector('#search_input')
   if (input) {
     input.addEventListener('keydown', function (e) {
       if (e.keyCode === 13) {
+        // submit
+        e.preventDefault()
         if (input.value) {
-          e.preventDefault()
           prepareSecondScreen()
         } else {
-          e.preventDefault()
           handleEmptyInput()
         }
+      } else {
+        // user is typing
+        adaptFontSize()
       }
     })
   }
--- public/src/javascripts/modules/firstScreenHelper.js
+++ public/src/javascripts/modules/firstScreenHelper.js
@@ -18,3 +18,27 @@ export const handleEmptyInput = function () {
     },0);
   }
 }
+
+export function adaptFontSize () {
+  // copy text input into hidden div
+  $('#hidden_helper_div').text($('#search_input').val())
+  // get div width => width of input
+  const inputWidth = $('#hidden_helper_div').width()
+  const wrapperWidth = $('#search_screen').width()
+
+  printDebug()
+  if (inputWidth > wrapperWidth - 50) {
+    const newFontSize = decreaseFontSize()
+
+    $('#search_input').css('font-size', newFontSize)
+    $('#hidden_helper_div').css('font-size', newFontSize)
+    adaptFontSize()
+  }
+}
+
+function decreaseFontSize () {
+  const oldFontSize = parseInt($('#search_input').css('font-size').slice(0,-2))
+  const newFontSize = (oldFontSize - (oldFontSize / 20))
+
+  return newFontSize.toString() + 'px'
+}
